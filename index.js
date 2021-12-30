974f6bca6fa1c2cad7b41ac248190a41af0439d1
yasinmillers <ssenyonga100@gmail.com>
2021-12-30 15:21:33 +0300

    houseney

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(() => console.log('db connected')).catch(err => console.log(err));
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
