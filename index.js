81f66542aec2fc276c1d720f956dc4742aaa8f55
yasinmillers <ssenyonga100@gmail.com>
2021-12-30 16:32:09 +0300

    get method

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoute = require('./routes/users');
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => console.log('db connected')).catch(err => console.log(err));
 
-app.get('/user', (req, res) => {
-    console.log('hello');
-});
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
