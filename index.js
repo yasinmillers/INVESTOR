f5a0352508d3e310765747584b29d91c116ebbc9
yasinmillers <ssenyonga100@gmail.com>
2021-12-30 15:52:10 +0300

    port changed

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const dotenv = require('dotenv');
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL).then(() => console.log('db connected')).catch(err => console.log(err));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
 })
