70f54af6bb8849a6f70c3c28b0b06a29ff93c6bf
yasinmillers <ssenyonga100@gmail.com>
2022-01-20 10:45:29 +0300

    changes made

--- routes/users.js
+++ routes/users.js
@@ -34,14 +34,47 @@ router.delete('/:id', verifyTokenAndAuthorization, async(req, res) => {
 
 //get user
 router.get('find/:id', verifyTokenAndAdmn, async(req, res) => {
+        try {
+            const User = await user.findById(req.params.id)
+            const { password, ...others } = user._doc;
+            res.status(200).json(others);
+
+
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    })
+    //get all users
+router.get('/:id', verifyTokenAndAdmn, async(req, res) => {
+        const query = req.query.new
+        try {
+            const User = query ?
+                await user.find().sort({ _id: -1 }).limit(5) :
+                await user.find();
+            // const { password, ...others } = user._doc;
+            res.status(200).json(user);
+
+
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    })
+    // user stats
+router.get('/stats', verifyTokenAndAdmn, async(req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
     try {
-        const User = await user.findById(req.params.id)
-        const { password, ...others } = user._doc;
-        res.status(200).json(others);
-
-
+        const data = await user.aggregate([
+            { $match: { createAt: { $gate: lastYear } } },
+            {
+                $project: {
+                    month: { $month: '$createdAt' },
+                }
+            }
+        ])
     } catch (err) {
         res.status(500).json(err)
+
     }
 })
 module.exports = router;
